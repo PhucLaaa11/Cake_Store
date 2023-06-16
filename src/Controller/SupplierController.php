b91e7ed839c24f86390150f42fb44317599fc159
Viet Tien <tientvgcc210283@fpt.edu.vn>
2023-06-16 13:45:37 +0700

    supplier create

--- src/Controller/SupplierController.php
+++ src/Controller/SupplierController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Supplier;
+use App\Form\SupplierType;
+use App\Repository\SupplierRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +17,26 @@ class SupplierController extends AbstractController
     {
         return $this->render('supplier/index.html.twig', [
             'controller_name' => 'SupplierController',
+        ]);
+    }
+    #[Route('/supplier/create', name: 'app_supplier_create', priority: 1)]
+    public function createAction(SupplierRepository $supplierRepository, Request $request): Response
+    {
+
+        $form = $this->createForm(SupplierType::class, new  Supplier());
+
+//        dd($request);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $supplier = $form->getData();
+            $supplierRepository->save($supplier, true);
+            $this->addFlash('success', 'Supplier\'s inserted successfully');
+            return $this->redirectToRoute('app_supplier_all');
+        }
+
+        return $this->render('supplier/create.html.twig', [
+            'form' => $form
         ]);
     }
 }
